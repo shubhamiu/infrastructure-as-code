a90a17bbed3f5819d2882533480d7ff9557e922a
shubhamiu <157778293+shubhamiu@users.noreply.github.com>
2024-04-18 20:13:39 -0400

    Add files via upload

--- resource-group.tf
+++ resource-group.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "practicum16" {
-    name = "practicum16"
-    location = "eastus"
+    name = var.app_name
+    location = var.location
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable app_name {
+    default = "practicum16"
+}
+
+variable app_name_container_registry {
+    default = "practicum16adcr"
+}
+
+variable location {
+    default = "eastus"
+}
+
+variable kubernetes_version {
+
+}
